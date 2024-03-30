0b37089d8544c75ac6871fd363a3cabfd374cfae
Hasnain saeed <ihasnain4@gmail.com>
2024-03-30 18:13:46 +0500

    add PWA integration

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="flex gap-5  items-center justify-start">
+        <Link
+          href={"/"}
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-3 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:py-3 lg:px-6 lg:dark:bg-zinc-800/30"
+        >
+          Home
+        </Link>
+        <Link
+          href={"/about"}
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-3 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:py-3 lg:px-6 lg:dark:bg-zinc-800/30"
+        >
+          Home
+        </Link>
+      </div>
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By{" "}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className="dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
